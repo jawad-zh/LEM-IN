94637cdee0937b4e38a5e2e0608c4ffaec3b4f96
jawad-zh <zahraoui.jawad20@gmail.com>
2025-09-09 13:10:48 +0100

    found all pahtes

--- functions/foundpath.go
+++ functions/foundpath.go
@@ -1,25 +1,35 @@
 package lemin
 
+import "fmt"
+
 func FoundPath(rooms map[string][]string, start string, end string) [][]string {
 	var group [][]string
-	var path []string
-	visited := make(map[string]bool)
-	queue := []string{start}
-	visited[start] = true
+	queue := [][]string{{start}}
 	for len(queue) > 0 {
 		room := queue[0]
 		queue = queue[1:]
-		path = append(path, room)
-		if room == end {
-			group = append(group, path)
+		last := room[len(room)-1]
+		if last == end {
+			group = append(group, room)
 			continue
 		}
-		for _, neighbor := range rooms[room] {
-			if !visited[neighbor] {
-				visited[neighbor] = true
-				queue = append(queue, neighbor)
+		for _, neighbor := range rooms[last] {
+			if visited(neighbor,room) {
+				newPath := append([]string{}, room...)
+				newPath = append(newPath, neighbor)
+				queue = append(queue, newPath)
 			}
 		}
 	}
+	fmt.Print(group)
 	return group
+}
+
+func visited(neighbor string, room []string) bool {
+	for _,r := range room {
+		if neighbor == r {
+			return false
+		}
+	}
+	return  true
 }

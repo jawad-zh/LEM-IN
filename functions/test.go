9de7464fca96f108cb32c9777f4be005cac13ae4
jawad-zh <zahraoui.jawad20@gmail.com>
2025-09-23 13:00:09 +0100

    allpahtes

--- functions/test.go
+++ functions/test.go
@@ -5,19 +5,26 @@ import "fmt"
 func Test(rooms map[string][]string, start, end string) [][]string {
 	var room []string
 	var group [][]string
+	var group2 [][]string
 	// var first bool
 	var children []string
 	children = rooms[start]
-	
+	// fmt.Println(rooms[start])
+	// fmt.Println(len(rooms[start]))
 	// fmt.Println(children)
 	i := 0
 	for i <= len(children)-1 {
-
-		group = append(group, Bfs(room, children[i], rooms, end)...)
-
+		
+		group = append(group,Bfs(room, children[i], rooms, end)...)
+		group2 = append(group2, group...)
+		
+		// fmt.Println("i:",i)
 		i++
+		// fmt.Println("i++:",i)
 	}
-	fmt.Println(group)
+	fmt.Println(group2)
+	fmt.Println(len(group2))
+	// fmt.Println("children:",children)
 	return group
 }
 
@@ -32,7 +39,6 @@ func Bfs(room []string, child string, rooms map[string][]string, end string) [][
 		last := room[len(room)-1]
 		if last == end {
 			group = append(group, room)
-			// room = nil
 			continue
 		}
 
